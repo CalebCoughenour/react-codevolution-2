9f82d03f5d085cb9df0a14694766423272e83d90
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-13 14:33:51 -0700

    demonstrate how to add refs to input fields

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 function App() {
   return (
     <div className="App">
-      <ParentComp />
+      <RefsDemo />
+      {/* <ParentComp /> */}
       {/* <Table /> */}
       {/* <FragmentDemo /> */}
     </div>
